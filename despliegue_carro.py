dde41af01c605f20dc2ca51097b339e2794e62a3
ingjfnf <161249718+ingjfnf@users.noreply.github.com>
2024-04-28 01:54:52 -0500

    Update despliegue_carro.py

--- despliegue_carro.py
+++ despliegue_carro.py
@@ -5,19 +5,19 @@ import joblib
 import sys
 import os
 
-def predict_price(df_test, row_index):
+def predict_price(df_test, fila):
     # Cargamos el modelo de predicción de precios y el transformador
     clf = joblib.load(os.path.join(os.path.dirname(__file__), 'carros.pkl'))
     transformador = joblib.load(os.path.join(os.path.dirname(__file__), 'transformador.pkl'))
     
     # Seleccionamos la fila que queremos predecir
-    input_data = df_test.iloc[[row_index]]
+    dato = df_test.iloc[[fila]]
     
     # Aplicamos la transformación al dato de entrada usando el transformador 
-    input_transformed = transformador.transform(input_data)
+    transformacion = transformador.transform(dato)
 
     # Hacemos la predicción
-    precio_predicho = clf.predict(input_transformed)
+    precio_predicho = clf.predict(transformacion)
 
     return precio_predicho
 
@@ -28,10 +28,10 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print('Por favor ingrese un número de ID para evaluar en el conjunto de TEST')
     else:
-        row_index = int(sys.argv[1])
-        p1 = predict_price(df_test, row_index)
+        fila = int(sys.argv[1])
+        p1 = predict_price(df_test, fila)
         
-        print(f'La predicción del precio del automóvil para el ID: {row_index} del conjunto de prueba es: {p1[0]}')
+        print(f'La predicción del precio del automóvil para el ID: {fila} del conjunto de prueba es: {p1[0]}')
 
 
         
