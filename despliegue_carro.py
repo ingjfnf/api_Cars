7c20ac5fe90db41e985da2f08e469a3ddbab6388
ingjfnf <161249718+ingjfnf@users.noreply.github.com>
2024-04-27 23:04:55 -0500

    Update despliegue_carro.py

--- despliegue_carro.py
+++ despliegue_carro.py
@@ -1,33 +1,19 @@
+#!/usr/bin/python
+
 import pandas as pd
 import joblib
-import numpy as np
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.compose import ColumnTransformer
 import sys
 import os
 
-# Definimos las columnas numéricas y categóricas como en el entrenamiento
-categoricas = ['State', 'Make', 'Model']
-numericas = ['Year', 'Mileage']
-
-# Transformador para preprocesar los datos de entrada
-# Este deberá ser idéntico al que se utilizó durante el entrenamiento
-transformador = ColumnTransformer(
-    transformers=[
-        ('num', StandardScaler(), numericas),
-        ('cat', OneHotEncoder(handle_unknown='ignore'), categoricas)
-    ],
-    remainder='drop'  # Descartar columnas que no sean numéricas ni categóricas
-)
-
 def predict_price(df_test, row_index):
-    # Cargamos el modelo de predicción de precios
+    # Cargamos el modelo de predicción de precios y el transformador
     clf = joblib.load(os.path.join(os.path.dirname(__file__), 'carros.pkl'))
-
+    transformador = joblib.load(os.path.join(os.path.dirname(__file__), 'transformador.pkl'))
+    
     # Seleccionamos la fila que queremos predecir
     input_data = df_test.iloc[[row_index]]
     
-    # Aplicamos la transformación al dato de entrada
+    # Aplicamos la transformación al dato de entrada usando el transformador ajustado
     input_transformed = transformador.transform(input_data)
 
     # Hacemos la predicción
@@ -37,7 +23,7 @@ def predict_price(df_test, row_index):
 
 if __name__ == "__main__":
     # Carga del conjunto de datos de prueba
-    df_test = pd.read_csv('https://raw.githubusercontent.com/davidzarruk/MIAD_ML_NLP_2023/main/datasets/dataTest_carListings.zip', index_col=0) # Asegúrate de tener el archivo en el directorio correcto
+    df_test = pd.read_csv('dataTest_carListings.csv', index_col=0)  # Cambia la ruta si el archivo está en otro lugar
 
     if len(sys.argv) == 1:
         print('Por favor ingrese un número de ID para evaluar en el conjunto de TEST')
@@ -46,6 +32,7 @@ if __name__ == "__main__":
         p1 = predict_price(df_test, row_index)
         
         print(f'La predicción del precio del automóvil para el ID: {row_index} del conjunto de prueba es: {p1[0]}')
+
 
         
         
