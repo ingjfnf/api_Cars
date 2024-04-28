bafd7fa3ce0ab404bd1ba6a8a464bdf67b674e78
ingjfnf <161249718+ingjfnf@users.noreply.github.com>
2024-04-27 22:16:44 -0500

    Update api_carros.py

--- api_carros.py
+++ api_carros.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_restx import Api, Resource, fields
 import joblib
-from despliegue_carro import predict_price 
+from despliegue_carro import predict_price  
 from flask_cors import CORS
 import pandas as pd  
 
 app = Flask(__name__)
-CORS(app)  # Habilita CORS para todas las rutas y orígenes
+CORS(app)  
 
 api = Api(
     app, 
@@ -39,15 +39,15 @@ class CarPriceApi(Resource):
         args = parser.parse_args()
         row_index = args['row_index']
         
-        
         try:
-            test_row = df_test.iloc[[row_index]]
-            prediction = predict_proba(test_row)
-            return {"result": prediction}, 200
+            # Asegúrate de que tu DataFrame 'df_test' es el correcto y está cargado
+            test_row = df_test.iloc[[row_index]].values.reshape(1, -1)  # Asumiendo que el modelo espera un array 2D
+            prediction = predict_price(test_row)  # Usamos la función correcta 'predict_price'
+            return {"result": prediction[0]}, 200  # Devolvemos el valor predicho, asumiendo que 'predict' devuelve un array
         except Exception as e:
             api.abort(404, f"Error: {str(e)}")
 
 if __name__ == '__main__':
-    # Cargar tu conjunto de datos de prueba aquí
+    # Cargamos el conjunto de datos de prueba desde una URL
     df_test = pd.read_csv('https://raw.githubusercontent.com/davidzarruk/MIAD_ML_NLP_2023/main/datasets/dataTest_carListings.zip', index_col=0)
     app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
